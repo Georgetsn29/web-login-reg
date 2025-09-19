bf835906e80f5f76c3854722e59007eceb52d9a5
Georgetsn29 <giotsnobiladze5@gmail.com>
2025-09-20 00:26:20 +0400

    m

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
+const repoName = "login-reg-react"; // your repo name
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",  // <-- replaces `next export`
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
+  trailingSlash: true,
 };
 
 export default nextConfig;
