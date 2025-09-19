7810a55c5433dfc0b48a06835d4b74833bbcc530
Georgetsn29 <giotsnobiladze5@gmail.com>
2025-09-20 01:04:27 +0400

    aa

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable}`}
+        style={{
+          backgroundColor: "var(--background)",
+          color: "var(--foreground)",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "100vh",
+        }}
+      >
         {children}
       </body>
     </html>
